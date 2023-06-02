363beb993afd85e5f5d2dd7d29dd782da180f7ca
Korogras <brandon_faubion@yahoo.com>
2023-06-01 19:46:45 -0500

    finished await code for the async functions

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const playerContainer = document.getElementById('all-players-container');
 const newPlayerFormContainer = document.getElementById('new-player-form');
 
 // Add your cohort name to the cohortName variable below, replacing the 'COHORT-NAME' placeholder
-const cohortName = 'YOUR COHORT NAME HERE';
+const cohortName = '2302-ACC-ET-WEB-PT-D';
 // Use the APIURL variable for fetch requests
 const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`;
 
@@ -12,7 +12,9 @@ const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`;
  */
 const fetchAllPlayers = async () => {
     try {
-
+        const players = await fetch(API_URL)
+        const formattedPlayers = await players.json()
+        console.log(formattedPlayers)
     } catch (err) {
         console.error('Uh oh, trouble fetching players!', err);
     }
@@ -20,7 +22,8 @@ const fetchAllPlayers = async () => {
 
 const fetchSinglePlayer = async (playerId) => {
     try {
-
+        const result = await fetch(`API_URL/${playerId}`)
+        console.log(result)
     } catch (err) {
         console.error(`Oh no, trouble fetching player #${playerId}!`, err);
     }
@@ -28,7 +31,21 @@ const fetchSinglePlayer = async (playerId) => {
 
 const addNewPlayer = async (playerObj) => {
     try {
-
+const newPlayer = await fetch(API_URL, {
+    method: 'POST',
+    body: json.stringify({
+        id: playerId,
+        name: name,
+        breed: breed,
+        status: status,
+        imageUrl: imageUrl,
+        createdAt: createdAt,
+        updatedAt: updatedAt,
+        teamId: teamId,
+        cohortId: cohortId
+    })
+})
+console.log(newPlayer)
     } catch (err) {
         console.error('Oops, something went wrong with adding that player!', err);
     }
@@ -36,6 +53,9 @@ const addNewPlayer = async (playerObj) => {
 
 const removePlayer = async (playerId) => {
     try {
+const remove = await fetch(`API_URL/${playerId}`, {
+    method: 'DELETE'
+})
 
     } catch (err) {
         console.error(
